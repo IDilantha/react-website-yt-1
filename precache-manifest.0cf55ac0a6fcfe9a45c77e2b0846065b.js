a6ab99ce1a19c7bb00c04690e12c80db30918862
IDilantha <isurudilantha@gmail.com>
2020-09-13 02:00:41 +0530

    Updates

---
+++ precache-manifest.0cf55ac0a6fcfe9a45c77e2b0846065b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d22b85e56738aac680093f70297782b0",
+    "url": "/trvl/index.html"
+  },
+  {
+    "revision": "84b2d8df9bffc5f487c0",
+    "url": "/trvl/static/css/main.133da1e0.chunk.css"
+  },
+  {
+    "revision": "b911d4d8e46560afd077",
+    "url": "/trvl/static/js/2.1bc31245.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/trvl/static/js/2.1bc31245.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84b2d8df9bffc5f487c0",
+    "url": "/trvl/static/js/main.261b8d70.chunk.js"
+  },
+  {
+    "revision": "c4a4038d2e37ed27cff9",
+    "url": "/trvl/static/js/runtime-main.2983c951.js"
+  }
+]);
